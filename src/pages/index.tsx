1cf9ee80239b6d0e554b271822256a37d1190875
jhonataT <jhonata.tenorio2526@gmail.com>
2023-05-02 08:58:29 -0300

    Finishing first components in home screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,27 @@
 import { CustomHead } from '@/components/Head';
+import styles from './home.module.scss';
 
 const Home = () => {
   return (
     <>
-      <CustomHead />
-      <main>
-        <h1>
-          body
-        </h1>
+      <CustomHead title="Home | NewsApp"/>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the
+            <span> React </span>
+            world.
+          </h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <img
+          src="/images/avatar.svg"
+          alt="Female figure programming a react application"
+        />
       </main>
     </>
   )
